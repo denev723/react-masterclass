48f04871fe94bb9892340dd5c356073af5942b9e
denev <denev723@gmail.com>
2022-03-16 16:22:33 +0900

    styled components-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
     display: flex;
+    background-color: ${(props) => props.theme.backgroundColor};
 `;
 const Box = styled.div`
+    //property 전달
     background-color: ${(props) => props.bgColor};
     width: 100px;
     height: 100px;
@@ -12,22 +14,121 @@ const Box = styled.div`
     align-items: center;
 `;
 
+// style extends
 const Circle = styled(Box)`
     border-radius: 50%;
 `;
 
 const Text = styled.span`
+    color: ${(props) => props.theme.textColor};
+`;
+
+const Btn = styled.button`
     color: white;
+    background-color: tomato;
+    border: 0;
+    border-radius: 5px;
+`;
+
+// attributes 추가
+const Input = styled.input.attrs({ required: true })`
+    background-color: tomato;
+`;
+
+// animation 추가 styled-components에서 keyframes 불러오기
+const ratationAnimation = keyframes`
+    0% {
+        transform: ratate(0deg);
+        border-radius: 0px;
+    }
+    50% {
+        border-radius: 100px;
+    }
+    100% {
+        transform: rotate(360deg);
+        border-radius: 0px;
+    }
+`;
+
+const Emoji = styled.span`
+    font-size: 36px;
+`;
+
+const AnimationBox = styled.div`
+    height: 200px;
+    width: 200px;
+    background-color: tomato;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${ratationAnimation} 1s linear infinite;
+
+    //Pseudo Selector
+    span {
+        font-size: 36px;
+
+        &:hover {
+            font-size: 48px;
+        }
+
+        &:active {
+            opacity: 0;
+        }
+    }
+`;
+
+const AnimationBox2 = styled.div`
+    height: 200px;
+    width: 200px;
+    background-color: tomato;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${ratationAnimation} 1s linear infinite;
+
+    ${Emoji}:hover {
+        font-size: 98px;
+    }
 `;
 
 function App() {
     return (
-        <Father>
-            <Box bgColor="teal">
-                <Text>Hello</Text>
-            </Box>
-            <Circle bgColor="tomato"></Circle>
-        </Father>
+        <div>
+            <Father as={"header"}>
+                <Box bgColor="teal">
+                    <Text>Hello</Text>
+                </Box>
+                <Circle bgColor="tomato"></Circle>
+            </Father>
+            <hr></hr>
+            <Father>
+                <Btn>Log in</Btn>
+                {/* 같은 style 컴포넌트에 다른 태그를 적용 */}
+                <Btn as={"a"} href="/">
+                    Log in
+                </Btn>
+            </Father>
+            <hr></hr>
+            <Father>
+                <Input />
+                <Input />
+            </Father>
+            <hr></hr>
+            <Father>
+                <AnimationBox>
+                    <span>🤩</span>
+                </AnimationBox>
+            </Father>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Father>
+                <AnimationBox2>
+                    <Emoji>🤩</Emoji>
+                </AnimationBox2>
+                <Emoji>🔥</Emoji>
+            </Father>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+// dark/light theme 을 사용하라면 property name이 같아야 함
+const darkTheme = {
+    textColor: "whitesmoke",
+    backgroundColor: "#111"
+};
+
+const lightTheme = {
+    textColor: "#111",
+    backgroundColor: "whitesmoke"
+};
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        {/* theme object 내용을 props로 전달해줄 수 있음 */}
+        <ThemeProvider theme={darkTheme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
