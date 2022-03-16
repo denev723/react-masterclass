82541cf09d3cc1cb52856f25bc2a27683495b3a1
denev <denev723@gmail.com>
2022-03-16 14:44:54 +0900

    styled components-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+    display: flex;
+`;
+const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Circle = styled(Box)`
+    border-radius: 50%;
+`;
+
+const Text = styled.span`
+    color: white;
+`;
+
 function App() {
     return (
-        <div>
-            <h1>Welcome</h1>
-        </div>
+        <Father>
+            <Box bgColor="teal">
+                <Text>Hello</Text>
+            </Box>
+            <Circle bgColor="tomato"></Circle>
+        </Father>
     );
 }
 
